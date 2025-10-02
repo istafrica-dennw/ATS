d87e58951f442adcfd40639e1ff0d8809b2f3f1e
JEAMI <jean.misigaro@ist.com>
2025-10-02 11:07:41 +0200

    Add UserIcon import to UserManagementPage

--- frontend/src/pages/admin/UserManagementPage.tsx
+++ frontend/src/pages/admin/UserManagementPage.tsx
@@ -14,6 +14,7 @@ import {
   PlusIcon,
   MagnifyingGlassIcon,
   XMarkIcon,
+  UserIcon,
 } from '@heroicons/react/24/outline';
 import { ChevronUpDownIcon, CheckIcon } from '@heroicons/react/20/solid';
 
